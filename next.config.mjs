9cce95369f5012d85a55edf9b0e523e6c3014ec4
Victor Okpukpan <vokpukpan@gmail.com>
2024-11-25 16:35:27 +0100

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
   images: {
-    domains: ['arweave.net'], // Add your allowed image domains here
+    domains: ['arweave.net'],
   },
 };
 
